6eec0e1e768197bc45c2099b7e05ca07ba0e091e
utuedey <utuedeyejosph@gmail.com>
2023-08-04 04:51:55 +0000

    add encrypt_password.py

---
+++ 0x00-personal_data/encrypt_password.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""
+encrypt_password module
+"""
+import bcrypt
+
+
+def hash_password(password):
+    """Returns a salted, hashed password"""
+    hashed_password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+    return hashed_password
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """Validate hased password
+    Args:
+      hashed_password: bytes type
+      password: string type
+    Returns:
+        bool
+    """
+    if bcrypt.checkpw(password.encode('utf-8'), hashed_password):
+        return True
+    return False
