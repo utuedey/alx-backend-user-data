a99cef68484b1de61d27ad4ca027f3a677a71387
utuedey <utuedeyejosph@gmail.com>
2023-08-10 20:59:27 +0100

    update SessionAuth

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -15,9 +15,16 @@ class SessionAuth(Auth):
         super().__init__()
 
     def create_session(self, user_id: str = None) -> str:
-        """Retrieves a user_id based on a session_id"""
+        """Creates a session_id for a user_id"""
         if user_id is not None and isinstance(user_id, str):
             session_id = uuid.uuid4()
             SessionAuth.user_id_by_session_id[session_id] = user_id
             return session_id
+        return None
+
+    def user_id_for_session_id(self, session_id: str = None) -> str:
+        """Retrieves a user ID based on a Session ID"""
+        if session_id is not None or isinstance(session_id, str):
+            user_id = SessionAuth.user_id_by_session_id.get(session_id)
+            return user_id
         return None
