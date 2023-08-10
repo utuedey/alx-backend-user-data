6dd71dae8d66bee26a5cc4242507d8a84cb011e8
utuedey <utuedeyejosph@gmail.com>
2023-08-10 20:39:20 +0100

    update SessionAuth class

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -2,9 +2,22 @@
 """
 Session Authentication
 """
+import uuid
 from .auth import Auth
 
 
 class SessionAuth(Auth):
     """Session Authentication session"""
-    pass
+    user_id_by_session_id = {}
+
+    def __init__(self):
+        """Initialization"""
+        super().__init__()
+
+    def create_session(self, user_id: str = None) -> str:
+        """Retrieves a user_id based on a session_id"""
+        if user_id is not None and isinstance(user_id, str):
+            session_id = uuid.uuid4()
+            SessionAuth.user_id_by_session_id[session_id] = user_id
+            return session_id
+        return None
