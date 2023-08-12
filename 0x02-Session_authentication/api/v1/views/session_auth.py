bff3d836d6dcab9b636d1476ebf17f5eec69a64a
utuedey <utuedeyejosph@gmail.com>
2023-08-13 00:10:05 +0100

    update session_auth.py

--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -30,7 +30,7 @@ def login() -> Tuple[str, int]:
     if len(users) <= 0:
         return jsonify({"error": "no user found for this email"}), 404
     if users[0].is_valid_password(password):
-        from ap.vi.app import auth
+        from api.v1.app import auth
         session_id = auth.create_session(getattr(users[0], 'id'))
         res = jsonify(users[0].to_json())
         res.set_cookie(os.getenv("SESSION_NAME"), session_id)
