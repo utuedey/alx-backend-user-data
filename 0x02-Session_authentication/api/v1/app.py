7db3d93a5493572a3ca53439564df2299d9a6cc4
utuedey <utuedeyejosph@gmail.com>
2023-08-12 23:08:35 +0100

    update app.py

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -49,24 +49,24 @@ def forbidden(error) -> str:
 def authenticate_user():
     """Authenticate a user before processing a request
     """
-    if auth is None:
-        return ""
-    else:
+    if auth:
         excluded_paths = [
             '/api/v1/status/',
             '/api/v1/unauthorized/',
             '/api/v1/forbidden/',
-            'api/v1/auth_session/login/']
+            'api/v1/auth_session/login/',
+        ]
         if auth.require_auth(request.path, excluded_paths):
+            user = auth.current_user(request)
+
             auth_header = auth.authorization_header(request)
             auth_session = auth.session_cookie(request)
-            user = auth.current_user(request)
-            request.current_user = auth.current_user(request)
+
             if auth_header is None and auth_session is None:
-                return None
                 abort(401)
             if user is None:
                 abort(403)
+            request.current_user = user
 
 
 if __name__ == "__main__":
