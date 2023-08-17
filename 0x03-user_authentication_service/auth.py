034b1d35338f13274fd0d2bc0761786a663da2aa
utuedey <utuedeyejosph@gmail.com>
2023-08-17 18:29:06 +0100

    modify auth

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -36,7 +36,7 @@ class Auth:
         user = None
         try:
             user = self._db.find_user_by(email=email)
-            if user is None:
+            if user is not None:
                 return bcrypt.checkpw(
                     password.encode("utf-8"),
                     user.hashed_password)
