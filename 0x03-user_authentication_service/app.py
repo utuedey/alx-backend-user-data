87415156744943021473b652964b0ff8bb91c2e1
utuedey <utuedeyejosph@gmail.com>
2023-08-20 16:14:27 +0100

    add logout func

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -3,7 +3,7 @@
 Basic Flask app
 """
 from flask import Flask, jsonify, abort
-from flask import request
+from flask import request, redirect
 from auth import Auth
 
 app = Flask(__name__)
@@ -49,6 +49,20 @@ def login() -> str:
     response.set_cookie("session_id", session_id)
 
     return response
+
+
+@app.route("/sessions", methods=["DELETE"], strict_slashes=False)
+def logout() -> str:
+    """DELETE /sessions
+    Return:
+      - Redirect to home
+    """
+    session_id = request.cookies.get("session_id")
+    user = AUTH.get_user_from_session_id(session_id)
+    if user is None:
+        abort(403)
+    Auth.destroy_session(user.id)
+    return redirect("/")
 
 
 if __name__ == "__main__":
